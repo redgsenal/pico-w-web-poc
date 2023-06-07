8cd8ef352e731b4c9dcba09e85990e9fde6c6448
Reggie <reggie.senal@gmail.com>
2023-06-07 20:34:21 +0800

    updates

--- lib/wificonfig.py
+++ lib/wificonfig.py
@@ -1,2 +1,2 @@
-wificonfig = {'ssid': 'Ikapati', 'pw' : 'CJ85c5RPaa7H'};
-#wificonfig = {'ssid': 'Magbabaya', 'pw' : 'ta0sasu1up'};
+#wificonfig = {'ssid': 'Ikapati', 'pw' : 'CJ85c5RPaa7H'};
+wificonfig = {'ssid': 'Magbabaya', 'pw' : 'ta0sasu1up'};
